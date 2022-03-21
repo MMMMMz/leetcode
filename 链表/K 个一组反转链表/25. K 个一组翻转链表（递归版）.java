8a5690eb529da7e8bb8a86346dbbc0ad1ef68df9
MaZhe <34254925+MMMMMz@users.noreply.github.com>
2022-03-21 13:27:01 +0800

    Create 25. K 个一组翻转链表（递归版）.java

---
+++ 链表/K 个一组反转链表/25. K 个一组翻转链表（递归版）.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if(head == null){
+            return null;
+        }
+        ListNode a = head;
+        ListNode b = head;
+        for(int i = 0 ; i < k ; i++)
+        {
+            if(b == null){
+                return head;
+            }
+            b = b.next;
+        }
+        ListNode newHead = reverse(a,b);
+        a.next = reverseKGroup(b, k);
+        return newHead;
+    }
+    public ListNode reverse(ListNode a, ListNode b) {
+        ListNode pre = null;
+        ListNode cur = a;
+        ListNode next = a;
+        while(cur != b)
+        {
+            next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+}
