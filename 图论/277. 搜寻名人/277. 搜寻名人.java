1cfcbe57dee0f50b4aee59019582a670b4231342
MaZhe <34254925+MMMMMz@users.noreply.github.com>
2022-06-22 13:39:58 +0800

    Create 277. 搜寻名人.java

---
+++ 图论/277. 搜寻名人/277. 搜寻名人.java
@@ -0,0 +1,25 @@
+/* The knows API is defined in the parent class Relation.
+      boolean knows(int a, int b); */
+
+public class Solution extends Relation {
+    public int findCelebrity(int n) {
+        int cand = 0; 
+        for(int i = 1 ; i < n ; i++)
+        {
+            if(knows(cand, i) || !knows(i, cand)){
+                cand = i;
+            }
+        }
+
+        for(int i = 0 ; i < n ; i++)
+        {
+            if(i != cand){
+                if(knows(cand, i) || !knows(i, cand)){
+                    return -1;
+                }
+            }
+        }
+
+        return cand;
+    }
+}
