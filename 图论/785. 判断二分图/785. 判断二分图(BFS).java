b657457ace37cdb72104ee21f13d429fac309d75
MaZhe <34254925+MMMMMz@users.noreply.github.com>
2022-06-24 14:23:59 +0800

    Update 785. 判断二分图(BFS).java

--- 图论/785. 判断二分图/785. 判断二分图(BFS).java
+++ 图论/785. 判断二分图/785. 判断二分图(BFS).java
@@ -1,41 +1,45 @@
 class Solution {
 
-    boolean res = true;
     boolean color[];
     boolean visited[];
+    boolean res = true;
 
     public boolean isBipartite(int[][] graph) {
         int n = graph.length;
         color = new boolean[n];
         visited = new boolean[n];
+
         for(int i = 0 ; i < n ; i++)
         {
             if(!visited[i]){
-                traverse(graph, i);
+                bfs(graph, i);
             }
         }
 
         return res;
     }
 
-    void traverse(int[][] graph, int s){
-        Queue<Integer> q = new LinkedList();
+void bfs(int[][] graph, int s){
+    Queue<Integer> q = new LinkedList<>();
+    visited[s] = true;
+    q.add(s);
 
-        visited[s] = true;
-        q.offer(s);
-        while(!q.isEmpty() && res){
-            int cur = q.poll();
-            for(int neighbor : graph[cur]){
-                if(!visited[neighbor]){
-                    color[neighbor] = !color[cur];
-                    visited[neighbor] = true;
-                    q.offer(neighbor);
-                } else {
-                    if(color[neighbor] == color[cur]){
-                        res = false;
-                    }
+    while(!q.isEmpty())
+    {
+        int cur = q.poll();
+        for(int neighbor : graph[cur]){
+            if(!visited[neighbor]){
+                visited[neighbor] = true;
+                color[neighbor] = !color[cur];
+                q.add(neighbor);
+            } else {
+                if(color[cur] == color[neighbor]){
+                    res = false;
+                    return;
                 }
-            }     
+            }
         }
     }
+}
+
 }
