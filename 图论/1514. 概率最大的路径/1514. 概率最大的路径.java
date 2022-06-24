c9603b0b509738583aa5b29bc39326fbc6b8d519
MaZhe <34254925+MMMMMz@users.noreply.github.com>
2022-06-24 21:48:55 +0800

    Update 1514. 概率最大的路径.java

--- 图论/1514. 概率最大的路径/1514. 概率最大的路径.java
+++ 图论/1514. 概率最大的路径/1514. 概率最大的路径.java
@@ -10,30 +10,28 @@ class Solution {
         }
     }
 
-     List<double[]>[] buildGraph(int n, int[][] edges, double[] succProb){
-         List<double[]>[] graph = new LinkedList[n];
-         for(int i = 0 ; i < n ; i++)
-         {
-             graph[i] = new LinkedList<>();
-         }
-        
+    List<double[]>[] buildGraph(int n, int [][] edges, double[] succProb){
+        List<double[]> graph[] = new LinkedList[n];
+        for(int i = 0 ;i < n ; i++)
+        {
+            graph[i] = new LinkedList<>();
+        }
+
         for(int i = 0 ; i < edges.length ; i++)
         {
-            graph[edges[i][0]].add(new double[]{(double) edges[i][1], succProb[i]});
-            graph[edges[i][1]].add(new double[]{(double) edges[i][0], succProb[i]});
+            graph[edges[i][0]].add(new double[] { (double)edges[i][1], succProb[i]});
+            graph[edges[i][1]].add(new double[] { (double)edges[i][0], succProb[i]});
         }
-         
+        return graph;
+    }
 
-         return graph;
-     }
-
-    double dijkstra(int start, int end, List<double[]>[] graph){
+    double dijkstra(List<double[]>[] graph, int start, int end){
         double probTo[] = new double[graph.length];
         Arrays.fill(probTo, -1);
         probTo[start] = 1;
 
-        Queue<State> pq = new PriorityQueue<>((a, b) -> { return Double.compare(b.probFromStart, a.probFromStart); });
-        pq.offer(new State(start, 1));
+        PriorityQueue<State> pq = new PriorityQueue<>((a, b) -> {return Double.compare(b.probFromStart, a.probFromStart); });
+        pq.add(new State(start, 1));
 
         while(!pq.isEmpty()){
             State cur = pq.poll();
@@ -49,12 +47,12 @@ class Solution {
             }
 
             for(double neighbor[] : graph[curId]){
-                int nextId = (int)neighbor[0];
-                double probToNext = probTo[curId] * neighbor[1];
+                int neighborId = (int) neighbor[0];
+                double neighborProbFromStart = neighbor[1] * probTo[curId];
 
-                if(probTo[nextId] < probToNext){
-                    probTo[nextId] = probToNext;
-                    pq.offer(new State(nextId, probToNext));
+                if(probTo[neighborId] < neighborProbFromStart){
+                    probTo[neighborId] = neighborProbFromStart;
+                    pq.add(new State(neighborId, neighborProbFromStart));
                 }
             }
         }
@@ -62,11 +60,10 @@ class Solution {
         return 0.0;
     }
 
-
     public double maxProbability(int n, int[][] edges, double[] succProb, int start, int end) {
+        
+        List<double[]> graph[] = buildGraph(n, edges, succProb);
 
-        List<double[]>[] graph = buildGraph(n, edges, succProb);
-
-        return dijkstra(start, end, graph);
+        return dijkstra(graph, start, end);
     }
 }
