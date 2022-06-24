6264350724df3c62369730cbc72200c12ad1f723
MaZhe <34254925+MMMMMz@users.noreply.github.com>
2022-06-24 14:24:37 +0800

    Update 785. 判断二分图.java

--- 图论/785. 判断二分图/785. 判断二分图.java
+++ 图论/785. 判断二分图/785. 判断二分图.java
@@ -1,39 +1,41 @@
 class Solution {
 
-    boolean res = true;
     boolean color[];
     boolean visited[];
+    boolean res = true;
 
     public boolean isBipartite(int[][] graph) {
         int n = graph.length;
         color = new boolean[n];
         visited = new boolean[n];
+
         for(int i = 0 ; i < n ; i++)
         {
             if(!visited[i]){
-                traverse(graph, i);
+                dfs(graph, i);
             }
         }
 
         return res;
     }
 
-    void traverse(int[][] graph, int s){
-        
-        if(!res){
+    void dfs(int[][] graph, int s){
+        if(visited[s] || !res){
             return;
         }
 
         visited[s] = true;
+
         for(int neighbor : graph[s]){
             if(!visited[neighbor]){
                 color[neighbor] = !color[s];
-                traverse(graph, neighbor);
+                dfs(graph, neighbor);
             } else {
-                if(color[neighbor] == color[s]){
+                if(color[s] == color[neighbor]){
                     res = false;
                 }
             }
         }
     }
+
 }
