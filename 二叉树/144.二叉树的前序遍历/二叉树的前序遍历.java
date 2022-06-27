9621aa735a7efdca5f2d2e640f02a615fcf186d1
MaZhe <34254925+MMMMMz@users.noreply.github.com>
2022-06-27 10:38:03 +0800

    Update 二叉树的前序遍历.java

--- 二叉树/144.二叉树的前序遍历/二叉树的前序遍历.java
+++ 二叉树/144.二叉树的前序遍历/二叉树的前序遍历.java
@@ -14,14 +14,22 @@
  * }
  */
 class Solution {
-    List<Integer> res = new LinkedList<Integer>();
+
+    List<Integer> res = new ArrayList();
+
     public List<Integer> preorderTraversal(TreeNode root) {
-        if(root == null){
-            return res;
-        }
-        res.add(root.val);
-        preorderTraversal(root.left);
-        preorderTraversal(root.right);
+        traverse(root);
+        
         return res;
+    }
+
+    void traverse(TreeNode root){
+        if(root == null){
+            return;
+        }
+        
+        res.add(root.val);
+        traverse(root.left);
+        traverse(root.right);
     }
 }
