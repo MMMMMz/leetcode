638e42a7634311b5ff10cbf54e45b76582c7893c
MaZhe <34254925+MMMMMz@users.noreply.github.com>
2022-06-27 10:36:01 +0800

    Update 翻转二叉树（递归）.java

--- 二叉树/226.翻转二叉树/翻转二叉树（递归）.java
+++ 二叉树/226.翻转二叉树/翻转二叉树（递归）.java
@@ -15,12 +15,21 @@
  */
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-        if(root == null) return null;
+        traverse(root);
+
+        return root;
+    }
+
+    void traverse(TreeNode root){
+        if(root == null){
+            return;
+        }
+
         TreeNode temp = root.left;
         root.left = root.right;
         root.right = temp;
-        invertTree(root.left);
-        invertTree(root.right);
-        return root;
+
+        traverse(root.left);
+        traverse(root.right);
     }
 }
