1911ffec5aae9f77472737151b71e95415459264
MaZhe <34254925+MMMMMz@users.noreply.github.com>
2022-06-27 13:46:51 +0800

    Update 填充每个节点的下一个右侧节点指针（递归）.java

--- 二叉树/116. 填充每个节点的下一个右侧节点指针/填充每个节点的下一个右侧节点指针（递归）.java
+++ 二叉树/116. 填充每个节点的下一个右侧节点指针/填充每个节点的下一个右侧节点指针（递归）.java
@@ -23,18 +23,21 @@ class Node {
 
 class Solution {
     public Node connect(Node root) {
-        if(root == null) return null;
-        mergeTwoNode(root.left, root.right);
+        if(root == null){
+            return root;
+        }
+        traverse(root.left, root.right);
         return root;
     }
-    void mergeTwoNode(Node left, Node right)
-    {
-        if(left == null || right == null){
-            return ;
+
+    void traverse(Node node1, Node node2){
+        if(node1 == null || node2 == null){
+            return;
         }
-        left.next = right;
-        mergeTwoNode(left.left, left.right);
-        mergeTwoNode(right.left, right.right);
-        mergeTwoNode(left.right, right.left);
+
+        node1.next = node2;
+        traverse(node1.left, node1.right);
+        traverse(node2.left, node2.right);
+        traverse(node1.right, node2.left);
     }
 }
