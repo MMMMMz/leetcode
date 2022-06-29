eb8c824572daf04e514d1b42b6a12eacba6967fb
MaZhe <34254925+MMMMMz@users.noreply.github.com>
2022-06-29 10:34:18 +0800

    Update 315. 计算右侧小于当前元素的个数.java

--- 二叉树/315. 计算右侧小于当前元素的个数/315. 计算右侧小于当前元素的个数.java
+++ 二叉树/315. 计算右侧小于当前元素的个数/315. 计算右侧小于当前元素的个数.java
@@ -1,53 +1,66 @@
 class Solution {
+
     class Pair{
         int id;
         int val;
-        Pair(int id, int val){
-            this.val = val;
+
+        public Pair(int id, int val){
             this.id = id;
+            this.val = val;
         }
     }
-    int[] count;
-    Pair[] temp;
+
+    Pair temp[];
+    int count[];
+
     public List<Integer> countSmaller(int[] nums) {
-        int numsLength = nums.length;
-        List<Integer> res = new ArrayList();
-        count = new int[numsLength];
-        Pair[] arr = new Pair[numsLength];
-        temp = new Pair[numsLength];
-        for(int i = 0 ; i < numsLength ; i++)
+        int n = nums.length;
+        List<Integer> res = new LinkedList<>();
+        temp = new Pair[n];
+        count = new int[n];
+        Pair arr[] = new Pair[n];
+        for(int i = 0 ; i < n ; i++)
         {
             arr[i] = new Pair(i, nums[i]);
         }
-        mergeSort(arr, 0, numsLength - 1);
-        for(int i = 0 ; i < count.length ; i++)
-            res.add(count[i]);
+
+        sort(arr, 0, n - 1);
+        for(int c : count){
+            res.add(c);
+        }
         return res;
     }
 
-    void mergeSort(Pair[] arr, int low, int high){
-        if(low == high) return;
-        int mid = low + (high - low) / 2;
-        mergeSort(arr, low, mid);
-        mergeSort(arr, mid + 1, high);
-        merge(arr, low, mid, high);
+    void sort(Pair[] arr, int low, int hig){
+        if(low == hig){
+            return;
+        }
+        int mid = low + (hig - low) / 2;
+        sort(arr, low, mid);
+        sort(arr, mid + 1, hig);
+        merge(arr, low, mid, hig);
     }
 
-    void merge(Pair[] arr, int low, int mid, int high){
-        for(int i = low ; i <= high ; i++) temp[i] = arr[i];
-        int i = low, j = mid + 1;
-        for(int p = low ; p <= high ; p++)
+    void merge(Pair[] arr, int low, int mid, int hig){
+        for(int i = low ; i <= hig ; i++){
+            temp[i] = new Pair(arr[i].id, arr[i].val);
+        }
+
+        int i = low;
+        int j = mid + 1;
+        for(int p = low ; p <=hig ; p++)
         {
             if(i == mid + 1){
                 arr[p] = temp[j++];
-            }else if(j == high + 1){
+            } else if(j == hig + 1){
                 arr[p] = temp[i++];
-                count[arr[p].id] += j - mid - 1;
-            } else if(temp[i].val <= temp[j].val){
-                arr[p] = temp[i++];
-                count[arr[p].id] += j - mid - 1;
-            } else {
+                count[arr[p].id] += j - (mid + 1);
+            } else if(temp[i].val > temp[j].val){
                 arr[p] = temp[j++];
+                
+            } else {
+                arr[p] = temp[i++];
+                count[arr[p].id] += j - (mid + 1);
             }
         }
     }
