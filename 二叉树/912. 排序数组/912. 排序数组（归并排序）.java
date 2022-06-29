03e2e6b3c8a9207693051f25c230c23115c97126
MaZhe <34254925+MMMMMz@users.noreply.github.com>
2022-06-29 09:55:54 +0800

    Update 912. 排序数组（归并排序）.java

--- 二叉树/912. 排序数组/912. 排序数组（归并排序）.java
+++ 二叉树/912. 排序数组/912. 排序数组（归并排序）.java
@@ -1,32 +1,43 @@
 class Solution {
+
     int[] temp;
+
     public int[] sortArray(int[] nums) {
+
         temp = new int[nums.length];
-        mergeSort(nums, 0, nums.length - 1);
+        sort(nums, 0, nums.length - 1);
         return nums;
     }
-    void mergeSort(int[] nums, int low, int high)
-    {
-        if(low == high) return;
-        int mid = low + (high - low) / 2;
-        mergeSort(nums, low, mid);
-        mergeSort(nums, mid + 1, high);
-        merge(nums, low, mid, high);
+
+    void sort(int[] nums, int low, int hig){
+        if(low == hig){
+            return;
+        }
+
+        int mid = low + (hig - low) / 2;
+        sort(nums, low, mid);
+        sort(nums, mid + 1, hig);
+        merge(nums, low, mid, hig);
     }
-    void merge(int[] nums, int low, int mid, int high)
-    {
-        for(int i = low ; i <= high ; i++) temp[i] = nums[i];
-        int i = low, j = mid + 1;
-        for(int p = low ; p <= high ; p++)
+
+    void merge(int[] nums, int low, int mid, int hig){
+        for(int i = low ; i <= hig ; i++)
+        {
+            temp[i] = nums[i];
+        }
+
+        int i = low;
+        int j = mid + 1;
+        for(int p = low ; p <= hig ; p++)
         {
             if(i == mid + 1){
                 nums[p] = temp[j++];
-            } else if(j == high + 1){
+            } else if(j == hig + 1){
                 nums[p] = temp[i++];
-            } else if(temp[i] < temp[j]){
-                nums[p] = temp[i++];
-            } else {
+            } else if(temp[i] > temp[j]){
                 nums[p] = temp[j++];
+            } else {
+                nums[p] = temp[i++];
             }
         }
     }
