5d5679e7070450d5712fe55682a388ddab127595
MaZhe <34254925+MMMMMz@users.noreply.github.com>
2022-09-16 10:15:37 +0800

    Create 剑指 Offer 22. 链表中倒数第k个节点.java

---
+++ 剑指offer/链表/剑指 Offer 22. 链表中倒数第k个节点/剑指 Offer 22. 链表中倒数第k个节点.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode getKthFromEnd(ListNode head, int k) {
+        ListNode p1 = head;
+        ListNode p2 = head;
+        for(int i = 0 ; i < k ; i++)
+        {
+            p1 = p1.next;
+        }
+        while(p1 != null){
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+
+        return p2;
+    }
+}
