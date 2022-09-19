8075b1d17e24b8934749d01e47e75f99b6cf20a5
MaZhe <34254925+MMMMMz@users.noreply.github.com>
2022-09-19 10:54:38 +0800

    Update 剑指 Offer 57. 和为s的两个数字.java

--- 剑指offer/数组/剑指 Offer 57. 和为s的两个数字/剑指 Offer 57. 和为s的两个数字.java
+++ 剑指offer/数组/剑指 Offer 57. 和为s的两个数字/剑指 Offer 57. 和为s的两个数字.java
@@ -1,14 +1,14 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+
+        //最大的加最小的都比target大，所以最大的数舍弃；最小的加最大的都比target小，所以最小的舍弃
         int[] res = new int[2];
         int slow = 0;
         int fast = nums.length - 1;
         while(slow < fast)
         {
             if(nums[slow] + nums[fast] == target){
-                res[0] = nums[slow];
-                res[1] = nums[fast];
-                break;
+                return new int[]{nums[slow], nums[fast]};
             }
             
             if(nums[fast] + nums[slow]> target){
@@ -19,6 +19,6 @@ class Solution {
             
         }
 
-        return res;
+         return null;
     }
 }
