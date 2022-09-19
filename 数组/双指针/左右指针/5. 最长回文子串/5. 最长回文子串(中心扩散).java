4e3da50f80517f97342e928bb72be83ea0c51aed
MaZhe <34254925+MMMMMz@users.noreply.github.com>
2022-09-19 14:10:38 +0800

    Create 5. 最长回文子串(中心扩散).java

---
+++ 数组/双指针/左右指针/5. 最长回文子串/5. 最长回文子串(中心扩散).java
@@ -0,0 +1,25 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        String res = "";
+
+        for(int i = 0 ; i < s.length() ; i++)
+        {
+            String s1 = palindrome(s, i, i);
+            String s2 = palindrome(s, i, i + 1);
+
+            res = s1.length() > res.length() ? s1 : res;
+            res = s2.length() > res.length() ? s2 : res;
+        }
+
+        return res;
+    }
+
+    public String palindrome(String s, int left, int right){
+        while(left >=0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+            left--;
+            right++;
+        }
+
+        return s.substring(left + 1, right);
+    }
+}
