8ba81eac28119e9c5c352b57fef1ff91631a329d
MaZhe <34254925+MMMMMz@users.noreply.github.com>
2022-09-19 10:52:18 +0800

    Update 剑指 Offer 57. 和为s的两个数字(HashMap).java

--- 剑指offer/数组/剑指 Offer 57. 和为s的两个数字/剑指 Offer 57. 和为s的两个数字(HashMap).java
+++ 剑指offer/数组/剑指 Offer 57. 和为s的两个数字/剑指 Offer 57. 和为s的两个数字(HashMap).java
@@ -1,9 +1,5 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-
-        //最大的加最小的都比target大，所以最大的数舍弃；最小的加最大的都比target小，所以最小的舍弃
-
-
         int[] res = new int[2];
         HashMap<Integer,Integer> map = new HashMap();
 
