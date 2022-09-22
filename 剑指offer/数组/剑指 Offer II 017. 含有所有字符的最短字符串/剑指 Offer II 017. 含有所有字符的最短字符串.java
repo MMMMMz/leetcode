f8fcacfbecd838da0e468c0e3b4cb09112a41c0b
MaZhe <34254925+MMMMMz@users.noreply.github.com>
2022-09-22 15:43:03 +0800

    Create 剑指 Offer II 017. 含有所有字符的最短字符串.java

---
+++ 剑指offer/数组/剑指 Offer II 017. 含有所有字符的最短字符串/剑指 Offer II 017. 含有所有字符的最短字符串.java
@@ -0,0 +1,49 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        Map<Character, Integer> need = new HashMap<>();
+        Map<Character, Integer> window = new HashMap<>();
+
+        int left = 0;
+        int right = 0;
+        int valid = 0;
+        int start = 0;
+        int len = Integer.MAX_VALUE;
+
+        for(int i = 0 ; i < t.length() ; i++)
+        {
+            char c = t.charAt(i);
+            need.put(c, need.getOrDefault(c, 0) + 1);
+        }
+
+        while(right < s.length()){
+            char c = s.charAt(right);
+            right++;
+
+            if(need.containsKey(c)){
+                window.put(c, window.getOrDefault(c, 0) + 1);
+                if(window.get(c).equals(need.get(c))){
+                    valid++;
+                }
+            }
+
+            while(valid == need.size())
+            {
+                if(right - left < len){
+                    start = left;
+                    len = right - left;
+                }
+
+                char d = s.charAt(left);
+                left++;
+                if(need.containsKey(d)){
+                    if(window.get(d).equals(need.get(d))){
+                        valid--;
+                    }
+                    window.put(d, window.getOrDefault(d, 0) - 1);
+                }
+            }
+        }
+
+        return len == Integer.MAX_VALUE ? "" : s.substring(start, start + len);
+    }
+}
