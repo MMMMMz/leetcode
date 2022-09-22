88d7eff506f92a961c0a02b09cccc5aee6cf2940
MaZhe <34254925+MMMMMz@users.noreply.github.com>
2022-09-22 12:00:00 +0800

    Create 剑指 Offer 48. 最长不含重复字符的子字符串.java

---
+++ 剑指offer/数组/剑指 Offer 48. 最长不含重复字符的子字符串/剑指 Offer 48. 最长不含重复字符的子字符串.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> windows = new HashMap<>();
+        int left = 0;
+        int right = 0;
+        int res = 0;
+
+        while(right < s.length())
+        {
+            char c = s.charAt(right);
+            right++;
+            windows.put(c, windows.getOrDefault(c, 0) + 1);
+
+            while(windows.get(c) > 1){
+                char d = s.charAt(left);
+                left++;
+                windows.put(d, windows.get(d) - 1);
+            }
+
+            res = Math.max(res, right - left);
+        }
+
+        return res;
+    }
+}
