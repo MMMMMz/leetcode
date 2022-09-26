c48d89355191e36b0649474dd32dda4777a7a337
MaZhe <34254925+MMMMMz@users.noreply.github.com>
2022-09-26 10:37:12 +0800

    Create 114. 二叉树展开为链表(分解子问题).java

---
+++ 二叉树/二叉树思路/114. 二叉树展开为链表/114. 二叉树展开为链表(分解子问题).java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void flatten(TreeNode root) {
+        traverse(root);
+    }
+
+    TreeNode traverse(TreeNode root){
+        if(root == null){
+            return root;
+        }
+
+        TreeNode left = traverse(root.left);
+        TreeNode right = traverse(root.right);
+        TreeNode p = left;
+        while(p != null && p.right != null){
+            p = p.right;
+        }
+        if(p != null){
+            p.right = right;
+            root.right = left;
+            root.left = null;
+        }
+        return root;
+    }
+}
