6bfe689f1ff3e92166ec9aa8f9e92706f0f99a8f
MaZhe <34254925+MMMMMz@users.noreply.github.com>
2023-02-07 11:07:41 +0800

    Create 3. 无重复字符的最长子串.java

---
+++ Hot100./3. 无重复字符的最长子串.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int left = 0;
+        int right = 0;
+        int res = 0;
+        Map<Character, Integer> window = new HashMap<>();
+        while(right < s.length()){
+            char c = s.charAt(right);
+            right++;
+            window.put(c, window.getOrDefault(c, 0)  + 1);
+
+            while(window.get(c) > 1){
+                char d = s.charAt(left);
+                left++;
+                window.put(d, window.get(d) -1);
+            }
+            res = Math.max(res, right - left);
+        }
+        return res;
+    }
+}
