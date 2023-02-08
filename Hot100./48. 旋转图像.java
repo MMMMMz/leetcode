7faf4ca7bd242600b35bd1b350972421e18e4fd7
MaZhe <34254925+MMMMMz@users.noreply.github.com>
2023-02-08 11:05:35 +0800

    Create 48. 旋转图像.java

---
+++ Hot100./48. 旋转图像.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for(int i = 0 ; i < n; i++)
+        {
+            for(int j = i + 1 ; j < n ; j++)
+            {
+                swap(matrix,i, j, j, i);
+            }
+        }
+        for(int i = 0 ; i < n ; i++)
+        {
+            for(int j = 0 ; j < n / 2 ; j++){
+                swap(matrix, i, j, i, n - 1 - j);
+            }
+            
+        }
+    }
+
+    void swap(int[][] matrix, int i, int j, int m, int n){
+        int temp = matrix[i][j];
+        matrix[i][j] = matrix[m][n];
+        matrix[m][n] = temp;
+    }
+}
