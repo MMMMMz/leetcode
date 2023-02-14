678c73b6d0d8515f0482e6164242c780ba44df51
MaZhe <34254925+MMMMMz@users.noreply.github.com>
2023-02-14 09:46:41 +0800

    Create 200. 岛屿数量.java

---
+++ Hot100./200. 岛屿数量.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int res = 0;
+        int m = grid.length;
+        int n = grid[0].length;
+
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(grid[i][j] == '1'){
+                    res++;
+                    dfs(grid, i, j, m, n);
+                }
+            }
+        }
+        return res;
+    }
+    void dfs(char[][] grid, int i, int j, int m, int n){
+
+        if(i < 0 || j < 0 || i >= m | j >= n) return;
+        if(grid[i][j] == '0') return;
+        grid[i][j] = '0';
+        int[][] dirs = new int[][]{{0,1},{0,-1},{1,0},{-1,0}}; 
+        for(int[] dir : dirs){
+            dfs(grid, i + dir[0], j + dir[1], m, n);
+        }
+    }
+}
