275c8b098983f58d55eb08105a0fed05c2605f8e
MaZhe <34254925+MMMMMz@users.noreply.github.com>
2023-02-16 13:18:37 +0800

    Create 448. 找到所有数组中消失的数字.java

---
+++ Hot100./448. 找到所有数组中消失的数字.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        int n = nums.length; 
+        List<Integer> res = new ArrayList<>();
+        HashSet<Integer> set = new HashSet<>();
+        for(int num : nums){
+            set.add(num);
+        }
+        for(int i = 1 ; i <= n ; i++){
+            if(!set.contains(i)){
+                res.add(i);
+            }
+        }
+        return res;
+    }
+}
