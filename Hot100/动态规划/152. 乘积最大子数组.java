bf7594609a42a7585e9aa2772d71a0e45172cc67
MaZhe <34254925+MMMMMz@users.noreply.github.com>
2023-02-23 09:20:34 +0800

    Create 152. 乘积最大子数组.java

---
+++ Hot100/动态规划/152. 乘积最大子数组.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int n = nums.length;
+        int[] max = new int[n];
+        int[] min = new int[n];
+        max[0] = nums[0];
+        min[0] = nums[0];
+        int res = nums[0];
+        for(int i = 1 ; i < n ; i++){
+            max[i] = Math.max(nums[i], Math.max(nums[i] * min[i - 1], nums[i] * max[i - 1]));
+            min[i] = Math.min(nums[i],Math.min(nums[i] * min[i - 1], nums[i] * max[i - 1]));
+            res = Math.max(res, max[i]);
+        }
+        return res;
+    }
+}
