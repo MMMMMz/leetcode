2e2253297509dd44d1b6e2479b36c852264cab8b
MaZhe <34254925+MMMMMz@users.noreply.github.com>
2023-02-24 14:26:34 +0800

    Create 647. 回文子串.java

---
+++ Hot100/回文串/647. 回文子串.java
@@ -0,0 +1,20 @@
+class Solution {
+
+    int res = 0;
+
+
+    public int countSubstrings(String s) {
+        for(int i = 0 ; i < s.length() ; i++){
+            traverse(s, i, i);
+            traverse(s, i , i + 1);
+        }
+        return res;
+    }
+
+    void traverse(String s, int left, int right){
+        while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+            left--;
+            right++;
+            res++;
+        }
+    }
