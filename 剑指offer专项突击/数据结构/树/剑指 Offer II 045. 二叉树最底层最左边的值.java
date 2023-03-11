ce1dc5ff3f8a19664024b88697cb932bff2945b8
MaZhe <34254925+MMMMMz@users.noreply.github.com>
2023-03-11 14:52:09 +0800

    Create 剑指 Offer II 045. 二叉树最底层最左边的值.java

---
+++ 剑指offer专项突击/数据结构/树/剑指 Offer II 045. 二叉树最底层最左边的值.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int findBottomLeftValue(TreeNode root) {
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        int res = root.val;
+        queue.add(root);
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for(int i = 0 ; i < size ; i++){
+                 TreeNode temp = queue.poll();
+                 if(i == 0){
+                     res = temp.val;
+                 }
+                 if(temp.left != null){
+                     queue.add(temp.left);
+                 }
+                 if(temp.right != null){
+                     queue.add(temp.right);
+                 }
+            }
+        }
+        return res;
+    }
+}
