129f16a8dcac0ec57585ff95e23ce4ca130c18b6
MaZhe <34254925+MMMMMz@users.noreply.github.com>
2023-03-15 18:45:47 +0800

    Create 337. 打家劫舍 III.java

---
+++ 专题/动态规划/337. 打家劫舍 III.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    Map<TreeNode, Integer> dp1;
+    Map<TreeNode, Integer> dp2;
+
+    public int rob(TreeNode root) {
+        dp1 = new HashMap<>();
+        dp2 = new HashMap<>();
+
+        traverse(root);
+
+        return Math.max(dp1.getOrDefault(root, 0), dp2.getOrDefault(root, 0));
+    }
+
+    void traverse(TreeNode root){
+        if(root == null){
+            return;
+        }
+
+        traverse(root.left);
+        traverse(root.right);
+        dp1.put(root, root.val + dp2.getOrDefault(root.left, 0) + dp2.getOrDefault(root.right, 0));
+        dp2.put(root, Math.max(dp1.getOrDefault(root.left, 0), dp2.getOrDefault(root.left, 0)) + Math.max(dp1.getOrDefault(root.right, 0), dp2.getOrDefault(root.right, 0)));
+    }
+}
